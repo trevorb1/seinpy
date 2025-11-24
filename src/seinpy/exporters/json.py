9b3335e48abe1e6f19226642ab0ae080f8abca9c
trevorb1 <trevor_barnes@sfu.ca>
2025-11-23 16:23:51 -0800

    json exporters

--- src/seinpy/exporters/json.py
+++ src/seinpy/exporters/json.py
@@ -1,5 +1,6 @@
 """Json writer."""
 
+import json
 from typing import List
 
 from seinpy.base import Exporter
@@ -9,5 +10,42 @@ from seinpy.schema import Episode
 class JsonExporter(Exporter):
     """Write the data to a json file."""
 
-    def write(self, data: List[Episode], save_path: str) -> None:
-        raise NotImplementedError
+    def convert_to_json(self, episodes: List[Episode] | Episode) -> list[dict]:
+        """Convert the data to a json string."""
+        if isinstance(episodes, Episode):
+            episodes = [episodes]
+        elif isinstance(episodes, List):
+            if not all(isinstance(episode, Episode) for episode in episodes):
+                raise ValueError("Data must be a list of episodes")
+        else:
+            raise ValueError("Data must be a list of episodes or a single episode")
+
+        all_data = []
+        for episode in episodes:
+            data = {}
+            episode_data = episode.model_dump()
+
+            # ref will be the same for all episodes
+            for _, values in episode_data.items():
+                # ref may not be present if credit or rating or data is empty
+                if values["ref"]:
+                    data["ref"] = values["ref"]
+                    continue
+
+            # remove ref from all other data
+            episode_data_copy = episode_data.copy()
+            for item in episode_data_copy:
+                episode_data[item].pop("ref")
+
+            # add all other data to the data dictionary
+            for key, value in episode_data.items():
+                data[key] = value
+
+            all_data.append(data)
+
+        return all_data
+
+    def export(self, data: List[Episode], save_path: str) -> None:
+        json_data = self.convert_to_json(data)
+        with open(save_path, "w") as f:
+            json.dump(json_data, f, indent=4, ensure_ascii=False)
---
+++ tests/exporters/test_json.py
@@ -0,0 +1,90 @@
+import pytest
+from seinpy.exporters.json import JsonExporter
+
+
+@pytest.fixture
+def expected_json():
+    return {
+        "ref": {
+            "episode_id": "S01E02",
+            "episode_num": 2,
+            "episode_title": "Episode 2.",
+        },
+        "script": {
+            "script_lines": [
+                {
+                    "speaker": "Jerry",
+                    "dialogue": "Hi, I'm Jerry.",
+                },
+                {
+                    "speaker": "George",
+                    "dialogue": "Hi, I'm George.",
+                },
+                {
+                    "speaker": "Kramer",
+                    "dialogue": "Hi, I'm Kramer.",
+                },
+                {
+                    "speaker": "Elaine",
+                    "dialogue": "Hi, I'm Elaine.",
+                },
+            ]
+        },
+        "credit": {
+            "description": "Seinfeld is literally a show about nothing.",
+            "date": "2025-01-01",
+            "writers": [
+                {
+                    "name": "Larry David",
+                },
+                {
+                    "name": "Jerry Seinfeld",
+                },
+            ],
+            "directors": [
+                {
+                    "name": "Jerry Seinfeld",
+                },
+                {
+                    "name": "Larry David",
+                },
+            ],
+            "actors": [
+                {
+                    "name": "Jerry Seinfeld",
+                    "role": "Jerry",
+                },
+                {
+                    "name": "Jason Alexander",
+                    "role": "George",
+                },
+                {
+                    "name": "Julia Louis-Dreyfus",
+                    "role": "Elaine",
+                },
+                {
+                    "name": "Michael Richards",
+                    "role": "Kramer",
+                },
+            ],
+        },
+        "rating": {
+            "rating": 10.0,
+            "num_votes": 100,
+            "link": "https://example.com",
+        },
+    }
+
+
+class TestJsonExporter:
+    def test_convert_to_json(self, fake_episode, expected_json):
+        actual = JsonExporter().convert_to_json(fake_episode)
+        expected = [expected_json]
+        assert actual == expected
+
+    def test_multiple_episodes_convert_to_json(self, fake_episode, expected_json):
+        actual = JsonExporter().convert_to_json([fake_episode, fake_episode])
+        expected = []
+        expected.append(expected_json)
+        expected.append(expected_json)
+        assert actual == expected
